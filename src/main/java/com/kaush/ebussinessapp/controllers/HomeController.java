01f0d9d529f9ea6673f115766c05eb5c8b7936ec
kaush <emailtokaush@gmail.com>
2021-01-03 00:59:16 -0500

    modify register api according to ui

--- src/main/java/com/kaush/ebussinessapp/controllers/HomeController.java
+++ src/main/java/com/kaush/ebussinessapp/controllers/HomeController.java
@@ -1,14 +1,52 @@
 package com.kaush.ebussinessapp.controllers;
 
+import javax.management.relation.RoleNotFoundException;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kaush.ebussinessapp.dtos.LoginDataDTO;
+import com.kaush.ebussinessapp.entities.User;
+import com.kaush.ebussinessapp.exceptions.UserNotFoundException;
+import com.kaush.ebussinessapp.services.UserService;
+
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	UserService userService;
 	
 	@RequestMapping("/")
 	public String home() {
 		return "index";
 	}
+	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("loginData",new LoginDataDTO());
+		return "login";
+	}
+	
+	@RequestMapping("/register")
+	public String register(Model model) {
+		model.addAttribute("formData",new User());
+		return "register";
+	}
+	
+	@RequestMapping("/saveUser")
+	public String registerUser(Model model, @Valid User user, Errors errors) throws RoleNotFoundException, UserNotFoundException {
+		if(errors.hasErrors()) {
+			return "/register";
+		}
+		userService.addUser(user);
+		return "showProducts";
+	}
+	
+	
 
 }
