8e677883676f889adcda21a481b904b2ccb522bf
kaush <emailtokaush@gmail.com>
2021-01-04 20:29:57 -0500

    remove unwanted comment

--- src/main/java/com/kaush/ebussinessapp/controllers/ProdController.java
+++ src/main/java/com/kaush/ebussinessapp/controllers/ProdController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kaush.ebussinessapp.entities.Cart;
 import com.kaush.ebussinessapp.entities.Product;
+import com.kaush.ebussinessapp.exceptions.ProductException;
 import com.kaush.ebussinessapp.services.CartService;
 import com.kaush.ebussinessapp.services.ProductService;
 
@@ -41,7 +44,6 @@ public class ProdController {
 	
 	@RequestMapping("/showAllProducts")
 	public String showAllProducts(Model model) {
-		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+productService.findAllProducts());
 		model.addAttribute("productList", productService.findAllProducts());
 		model.addAttribute("cart", new Cart());
 		return "showProducts";
@@ -55,6 +57,14 @@ public class ProdController {
 		
 		cartService.addToCart(cart, product);
 		return "showProducts";
+	}
+	
+	@GetMapping("/viewProduct/{id}")
+	public String viewProductById(Model model,@PathVariable Integer id) throws ProductException {
+		Product prod = productService.findProductById(id);
+		model.addAttribute("product", prod);
+		return "showProducts";
+		
 	}
 	
 }
